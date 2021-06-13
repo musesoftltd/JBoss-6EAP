093b149d740096b7baf6e2d06e7ccae846cd85fb
x <a@b.com>
2021-06-13 23:33:53 +0100

    auditing refactor. Reporting Object.

--- scripts/environmentScripts/localhost/auditEstateLocalhostMarketingDMZ.py
+++ scripts/environmentScripts/localhost/auditEstateLocalhostMarketingDMZ.py
@@ -6,10 +6,10 @@ Created on 11 Oct 2016
  
 from environmentProperties.localhost.inventory import servers as localServers
 from environmentProperties.localhost.properties import dictionary as localServerDictionary
-from library.auditing.auditingLibrary import auditInitAudit
+from library.auditing.auditingLibrary import ReportingObject
 from library.pega.auditServers_MarketingDMZ import auditServersMarketingDMZ
 
-auditInitAudit("localMachine", "pegaDMZ")
+ReportingObject.reportInitReport("localMachine", "pegaDMZ")
 
 # whether to allow the auditing framework to make changes to correct its findings...
 applyChanges = False
