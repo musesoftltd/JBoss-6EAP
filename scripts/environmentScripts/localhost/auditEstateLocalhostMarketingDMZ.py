64ca7693bdf102225fe09e1a72427663923393da
x5user <x5user@x5user.nubinet>
2022-07-12 11:53:39 +0100

    fixup reporting

--- scripts/environmentScripts/localhost/auditEstateLocalhostMarketingDMZ.py
+++ scripts/environmentScripts/localhost/auditEstateLocalhostMarketingDMZ.py
@@ -6,10 +6,10 @@ Created on 11 Oct 2016
  
 from environmentProperties.localhost.inventory import servers as localServers
 from environmentProperties.localhost.properties import dictionary as localServerDictionary
-from library.auditing.auditingLibrary import auditInitAudit
+from library.auditing.auditingLibrary import reportingObject
 from library.pega.auditServers_MarketingDMZ import auditServersMarketingDMZ
 
-auditInitAudit("localMachine", "pegaDMZ")
+reportingObject.reportInitReport("localMachine", "pegaDMZ")
 
 # whether to allow the auditing framework to make changes to correct its findings...
 applyChanges = False
